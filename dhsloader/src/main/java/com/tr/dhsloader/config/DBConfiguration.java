11e95464a2cc71ec61959df0a999bd4bd749aa87
vadapalliranganath <34186907+vadapalliranganath@users.noreply.github.com>
2017-12-16 21:49:20 +0530

    db config changes

--- dhsloader/src/main/java/com/tr/dhsloader/config/DBConfiguration.java
+++ dhsloader/src/main/java/com/tr/dhsloader/config/DBConfiguration.java
@@ -7,19 +7,29 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+/**
+ * @author Ram
+ * 
+ */
 @Configuration
 @EnableTransactionManagement
+@PropertySource("classpath:db.properties")
 public class DBConfiguration {
 
 	@Autowired
 	private Environment env;
 
+	/**
+	 * 
+	 * @return
+	 */
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
@@ -30,20 +40,21 @@ public class DBConfiguration {
 		return dataSource;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	@Bean
 	public LocalSessionFactoryBean sessionFactory() {
 		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
 
 		sessionFactory.setDataSource(dataSource());
 
-		// Classpath scanning of @Component, @Service, etc annotated class
 		sessionFactory.setPackagesToScan(env.getProperty("db.hibernate.packagesToScan"));
 
-		// Hibernate properties
 		Properties hibernateProperties = new Properties();
 		hibernateProperties.put("hibernate.dialect", env.getProperty("db.hibernate.dialect"));
 		hibernateProperties.put("hibernate.show_sql", env.getProperty("db.hibernate.show_sql"));
-		hibernateProperties.put("hibernate.format_sql", env.getProperty("db.hibernate.format_sql"));
 		hibernateProperties.put("hibernate.hbm2ddl.auto", env.getProperty("db.hibernate.hbm2ddl.auto"));
 
 		sessionFactory.setHibernateProperties(hibernateProperties);
@@ -51,6 +62,10 @@ public class DBConfiguration {
 		return sessionFactory;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	@Bean
 	public HibernateTransactionManager transactionManager() {
 		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
