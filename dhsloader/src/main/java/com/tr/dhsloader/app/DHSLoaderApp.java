379a5e149c29a3cb3d9211f82b27031cffc5f1a7
vadapalliranganath <34186907+vadapalliranganath@users.noreply.github.com>
2017-12-16 21:48:39 +0530

    app modified

--- dhsloader/src/main/java/com/tr/dhsloader/app/DHSLoaderApp.java
+++ dhsloader/src/main/java/com/tr/dhsloader/app/DHSLoaderApp.java
@@ -3,13 +3,10 @@ package com.tr.dhsloader.app;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.ApplicationContext;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 import com.tr.dhsloader.service.IDHSLoaderService;
 
@@ -17,31 +14,36 @@ import com.tr.dhsloader.service.IDHSLoaderService;
  * @author Ram
  * 
  */
-@Configuration
-@EnableAutoConfiguration
 @SpringBootApplication
 @ComponentScan({ "com.tr.dhsloader" })
-@EntityScan("com.tr.dhsloader")
-public class DHSLoaderApp {
+public class DHSLoaderApp implements CommandLineRunner {
 
 	@Autowired
 	private IDHSLoaderService service;
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(DHSLoaderApp.class);
 
+	/**
+	 * 
+	 * @param args
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
+		SpringApplicationBuilder applicationBuilder = new SpringApplicationBuilder();
+		applicationBuilder.sources(DHSLoaderApp.class);
+		applicationBuilder.web(false);
+		applicationBuilder.run(args);
 
-		ApplicationContext context = SpringApplication.run(DHSLoaderApp.class, args);
-
-		DHSLoaderApp test = context.getBean(DHSLoaderApp.class);
-		String filePath = "D:\\MIFID.REF.296E.20171129.82.1.1.txt.zip";
-
-		LOGGER.info("File path is {} ", filePath);
-
-		test.invoke(filePath);
 	}
 
-	private void invoke(String filePath) throws Exception {
+	/**
+	 * 
+	 */
+	@Override
+	public void run(String... arg0) throws Exception {
+		String filePath = "D:\\MIFID.REF.296E.20171129.82.1.1.txt.zip";
+		LOGGER.info("File path is {} ", filePath);
 		service.processReport(filePath);
+		LOGGER.info("Process end..");
 	}
 }
